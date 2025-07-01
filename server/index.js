cc2071ad627c9f271f7e9a818a607aebc7f0b047
sakshisinha-13 <118090835+sakshisinha-13@users.noreply.github.com>
2025-07-01 20:53:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,9 +18,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://frontend:3000",
-      // "https://c-playground.vercel.app"
-      "https://cp-final-rosy.vercel.app",
-      "https://cp-final-tbia-rg5u15d1f-sakshisinha-13s-projects.vercel.app",
+      "https://cp-final-dashboard.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
