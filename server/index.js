5d9180366db7f7c8dfc3ca87039528ea38605f8b
sakshisinha-13 <118090835+sakshisinha-13@users.noreply.github.com>
2025-07-01 20:56:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 const app = express();
